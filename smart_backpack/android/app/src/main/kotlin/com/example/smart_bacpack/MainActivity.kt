4ab97748581f11add72fd6aa7eb9d64965693a14
yasithppasindu <164892806+yasithh1@users.noreply.github.com>
2025-05-18 22:21:25 +0530

    Delete smart_backpack directory

--- smart_backpack/android/app/src/main/kotlin/com/example/smart_bacpack/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.smart_bacpack
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- smart_backpack/lib/services/firebase_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_database/firebase_database.dart';
-
-class FirebaseService {
-  final DatabaseReference _database = FirebaseDatabase.instance.ref();
-
-  Stream<DatabaseEvent> getBackpackDataStream() {
-    return _database.child('backpack').onValue;
-  }
-}
